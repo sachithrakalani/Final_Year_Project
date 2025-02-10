d5ea45ee7871463c8d0798805bd49c7093da7969
sachithrakalani <kalanisathyangi@gmail.com>
2025-02-10 14:25:02 +0530

    Style Review home page

--- src/component/ReviewHomePage.jsx
+++ src/component/ReviewHomePage.jsx
@@ -1,14 +1,16 @@
+import style from "./reviewhomepage.module.css";
+
 export default function ReviewHomePage() {
   return (
     <div>
-      <div>
+      <div className={style.main}>
         <center>
-          <h2>Review Gard</h2>
+          <h2 className={style.title}>Review Gard</h2>
           <label htmlFor="text">Past the Review</label>
           <input></input>
-          <button>Submit</button>
-          <lable>Result</lable>
-          <input></input>
+          <button className={style.button}>Submit</button>
+          <lable className={style.resultlable}>Result</lable>
+          <input className={style.resultinputfield}></input>
         </center>
       </div>
     </div>
