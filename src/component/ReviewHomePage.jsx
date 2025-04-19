375d39ee9d8a390546057718ee4fea2d972e12a3
sachithrakalani <kalanisathyangi@gmail.com>
2025-04-19 17:30:51 +0530

    Added a Clear button for remove the review text

--- src/component/ReviewHomePage.jsx
+++ src/component/ReviewHomePage.jsx
@@ -1,20 +1,27 @@
 import style from "./reviewhomepage.module.css";
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
 
 export default function ReviewHomePage() {
-  const [review, setReview] = useState('');
-  const [result, setResult] = useState('');
+  const [review, setReview] = useState("");
+  const [result, setResult] = useState("");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('http://localhost:3000/api/review', { review });
-      setResult(response.data.result); 
-      setReview('');
+      const response = await axios.post("http://localhost:3000/api/review", {
+        review,
+      });
+      console.log({ Response: response });
+      setResult(response.data.data.result);
     } catch (err) {
       console.log(err);
     }
+  };
+  const handleClear = (e) => {
+    e.preventDefault();
+    setReview("");
+    setResult("");
   };
 
   return (
@@ -22,15 +29,29 @@ export default function ReviewHomePage() {
       <div className={style.main}>
         <center>
           <h2 className={style.title}>Review Guard</h2>
-          <form onSubmit={handleSubmit}> 
-            <label htmlFor="review" className={style.reviewlable}>Paste the Review</label>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="review" className={style.reviewlable}>
+              Paste the Review
+            </label>
             <textarea
               id="review"
               className={style.reviewtextfield}
               value={review}
               onChange={(e) => setReview(e.target.value)}
             ></textarea>
-            <button className={style.button} type="submit">Submit</button>
+            <div className={style.buttons}>
+              <button className={style.button} type="submit">
+                Submit
+              </button>
+              <button
+                className={style.button}
+                type="button"
+                onClick={handleClear}
+              >
+                Clear
+              </button>
+            </div>
+
             <label htmlFor="result" className={style.resultlable}>
               Result
             </label>
