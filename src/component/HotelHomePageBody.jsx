301cf8ec766a1e15ffc1af7405e2da66444bd9cb
sachithrakalani <kalanisathyangi@gmail.com>
2025-05-20 18:39:55 +0530

    Style Hotel home page body

--- src/component/HotelHomePageBody.jsx
+++ src/component/HotelHomePageBody.jsx
@@ -1,34 +1,44 @@
+import { Link } from "react-router-dom";
 import img1 from "./../../public/assets/Gallefacehotel.jpg";
 
-export default function HotelHomePageBody() {
+
+export default function HomePageBody() {
   return (
     <div>
-      <div>
-        <div>
-          <img src={img1} alt="img1" />
-        </div>
-        <div>
-          <h2>Galle Face Hotel</h2>
-          <div>
-            <h2>$580</h2>
+      <div className="flex flex-row p-[20px]">
+        <div className="flex flex-col">
+          <div className="px-[50px] m-[20px]">
+            <img className="w-xs  rounded-2xl" src={img1} alt="img1" />   
+          </div>
+          <div className="px-[70px] top-[1%]">
+            <h2 className="flex items-center gap-2 text-lg font-semibold">
+              Galle Face Hotel
+            </h2>
+            <div>
+              <h2 className="font-semibold">$580</h2>
+            </div>
           </div>
         </div>
+
+        <div className="flex flex-col">
+          <div className="px-[50px] m-[20px]">
+            <img className="w-xs  rounded-2xl" src={img1} alt="img1" />
+          </div>
+          <div className="px-[70px] top-[1%]">
+            <h2 className="flex items-center gap-2 text-lg font-semibold">
+              Galle Face Hotel
+            </h2>
+            <div>
+              <h2 className="font-semibold">$580</h2>
+            </div>
+          </div>
+        </div>
+
       </div>
-      <div>
-        <h2>Total 60 items</h2>
-        <button>
-          <i className="fas fa-arrow-left"></i>
-        </button>
-        <button></button>
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>4</button>
-        <button>5</button>
-        <button>
-          <i className="fas fa-arrow-right"></i>
-        </button>
-      </div>
+      
     </div>
   );
 }
+
+
+
