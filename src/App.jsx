68bd3e84bab8e13281845bae534bea6c94762026
sachithrakalani <kalanisathyangi@gmail.com>
2025-05-21 20:05:07 +0530

    Fixed App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,3 @@
-// import HotelHomePageBody from "./component/HotelHomePageBody";
-// import HotelHomePage from "./component/HotelHomePage";
-// import HotelHomePageHeader from "./component/HotelHomePageHeader";
-// import ReviewHomePage from "./component/ReviewHomePage";
-// import SingleHotelDetailPage1 from "./component/SingleHotelDetailPage1";
-
-// function App() {
-//   return (
-//     <div className="App">
-//       {/* <ReviewHomePage /> */}
-//       <HotelHomePageHeader />
-//       {/* <HotelHomePage /> */}
-//      {/* < HotelHomePageBody/> */}
-//      <SingleHotelDetailPage1 />
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-
 import HotelHomePageBody from "./component/HotelHomePageBody";
 import HotelHomePage from "./component/HotelHomePage";
 import HotelHomePageHeader from "./component/HotelHomePageHeader";
